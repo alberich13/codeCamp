268aee52edb441ac7ebfe82fcf047ba8238184ea
Alejandro Paredes <manuel.paredes@dsindigo.com>
2019-04-25 13:32:06 -0500

    Obtiene una lista de seguidores

--- codecamp-twiiter/src/main/java/com/indigo/twiiter/service/TwiiterService.java
+++ codecamp-twiiter/src/main/java/com/indigo/twiiter/service/TwiiterService.java
@@ -1,5 +1,6 @@
 package com.indigo.twiiter.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
+import twitter4j.PagableResponseList;
+import twitter4j.User;
 
 @Slf4j
 @Component
@@ -33,7 +36,21 @@ public class TwiiterService {
 	}
 	
 	public List<String> getUserFollowers(String userName){
-		return null;
+		PagableResponseList<User> users = null;
+		List<String> followers = new ArrayList<String>();
+		long count = -1;
+		try {
+			do {
+				users = twitter.getFollowersList(userName, count);
+				for(User user :users) {
+					followers.add(user.getName());
+				}
+			} while((count = users.getNextCursor()) != 0);
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return followers;
 	}
 	
 	public List<String> getCommonFriends(List<String> userNameList){
--- codecamp-twiiter/src/test/java/com/indigo/twiiter/test/TwiiterTest.java
+++ codecamp-twiiter/src/test/java/com/indigo/twiiter/test/TwiiterTest.java
@@ -12,6 +12,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.indigo.twiiter.TwiiterConfig;
 import com.indigo.twiiter.service.TwiiterService;
 import lombok.extern.slf4j.Slf4j;
+import twitter4j.PagableResponseList;
+import twitter4j.User;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -26,6 +28,12 @@ public class TwiiterTest {
 	@Test
 	public void sendSaveJmsList() throws InterruptedException {
 		List<String> userFollowerList = twiiterService.printTest();
+		userFollowerList.forEach(log::info);
+	}
+	
+	@Test
+	public void getingFollowersByUser() throws InterruptedException {
+		List<String> userFollowerList = twiiterService.getUserFollowers("alemanuchau");
 		userFollowerList.forEach(log::info);
 	}
 }
