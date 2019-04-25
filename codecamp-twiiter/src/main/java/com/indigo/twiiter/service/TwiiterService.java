860e28dceff7d14e4a6806c3da551812273d48b2
Aaron <alberich13@gmail.com>
2019-04-25 12:26:27 -0500

    Adding Service test

--- codecamp-twiiter/src/main/java/com/indigo/twiiter/service/TwiiterService.java
+++ codecamp-twiiter/src/main/java/com/indigo/twiiter/service/TwiiterService.java
@@ -1,16 +1,21 @@
 package com.indigo.twiiter.service;
 
+import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import twitter4j.Twitter;
 
+@Component
 public class TwiiterService {
 	
 	@Autowired
 	private Twitter twitter;
 	
 	public List<String> getUserFollowers(String userName){
-		return null;
+		List<String> userFollowers = Arrays.asList(new String[]{"UNO", "DOS"});;
+		return userFollowers;
 	}
 	
 	public List<String> getCommonFriends(List<String> userNameList){
---
+++ codecamp-twiiter/src/test/java/com/indigo/twiiter/test/TwiiterTest.java
@@ -0,0 +1,31 @@
+package com.indigo.twiiter.test;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.indigo.twiiter.TwiiterConfig;
+import com.indigo.twiiter.service.TwiiterService;
+import lombok.extern.slf4j.Slf4j;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+@ContextHierarchy({
+@ContextConfiguration(classes = {TwiiterConfig.class})})
+public class TwiiterTest {
+	
+	@Autowired
+	private TwiiterService twiiterService;
+	
+	@Test
+	public void sendSaveJmsList() throws InterruptedException {
+		List<String> userFollowerList = twiiterService.getUserFollowers("Aaron");
+		userFollowerList.forEach(log::info);
+	}
+}
